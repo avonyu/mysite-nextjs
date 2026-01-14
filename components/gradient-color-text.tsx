8e4a758e2f70e5474c837bdcd672bee28617755e
Foryoung Yu <fuyang_yu@outlook.com>
2026-01-14 20:59:56 +0800

    feat: enhance About page with new content, improve layout and styling, and update GradientColorText component

--- components/gradient-color-text.tsx
+++ components/gradient-color-text.tsx
@@ -1,19 +1,26 @@
 import { cn } from "@/lib/utils";
 
+/**
+ * text with gradient color
+ * see: https://tailwindcss.com/docs/background-image#adding-a-linear-gradient
+ * @param gradientColor background-image with gradient color
+ * @returns JSX.Element
+ */
 function GradientColorText({
   gradientColor = "bg-linear-to-r from-blue-500 to-teal-400",
   className,
   ...props
-}: React.ComponentProps<"div"> & { gradientColor: string }) {
+}: React.ComponentProps<"div"> & { gradientColor?: string }) {
   return (
-    <span
+    <div
       className={cn(
+        "inline-block",
         "bg-clip-text text-transparent animate-fade-in",
         gradientColor,
         className
       )}
       {...props}
-    ></span>
+    />
   );
 }
 
--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export default async function Header() {
   return (
     <header
       className={cn(
-        "sticky top-0 w-full flex justify-between p-4 bg-white border-b border-gray-200",
+        "sticky top-0 w-full flex justify-between p-4 bg-white border-b border-gray-200 z-999",
         "dark:border-gray-800 dark:bg-gray-950"
       )}
     >
